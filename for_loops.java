4d5b1e993767749750b5a0ec92b69188d90baecd
Rishabh Srivastava <rishabhchandra2408@gmail.com>
2020-11-01 17:29:59 +0530

    Updated for_loops with new algos

--- for_loops.java
+++ for_loops.java
@@ -5,6 +5,8 @@ class for_loops {
     // for loop  
     for (int i = 1; i <= n; ++i) {
       System.out.println("Java is fun");
+	System.out.println("Done with for loops");
+		System.out.println("wait what about while loops");
     }
   }
 }
